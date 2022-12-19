50729350373f51852d5308477488788b0e105b87
[AlexeyMaiatnikov] <heldinbox87@gmail.com>
2022-12-19 22:29:32 +0800

    add ex008 009 010 011

---
+++ Examples010_MethotArray/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = { 1, 2, 3, 4, 11, 5, 6, 11, 7, 8, 9, 10, 11 };
+int n = array.Length;
+int find = 11;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
